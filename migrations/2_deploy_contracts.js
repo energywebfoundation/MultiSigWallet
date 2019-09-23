cd6f687717138e71aa285c7d685fbc0990ce5f42
ngyam <ngy.am.zs@gmail.com>
2019-09-23 14:42:36 +0200

    feat(ewc-support): migration to ewf chains
    - ewf, volta networks
    - network selection considered in migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,14 +7,14 @@ module.exports = deployer => {
   if (process.env.DEPLOY_FACTORY){
     deployer.deploy(MultisigWalletFactory)
     console.log("Factory with Daily Limit deployed")
-  } else if (args.length < 5) {
+  } else if (args.length < 7) {
     console.error("Multisig with daily limit requires to pass owner " +
       "list, required confirmations and daily limit")
-  } else if (args.length < 6) {
-    deployer.deploy(MultisigWalletWithoutDailyLimit, args[3].split(","), args[4])
+  } else if (args.length < 9) {
+    deployer.deploy(MultisigWalletWithoutDailyLimit, args[5].split(","), args[6])
     console.log("Wallet deployed")
   } else {
-    deployer.deploy(MultisigWalletWithDailyLimit, args[3].split(","), args[4], args[5])
+    deployer.deploy(MultisigWalletWithDailyLimit, args[5].split(","), args[6], args[7])
     console.log("Wallet with Daily Limit deployed")
   }
 }
--- truffle.js
+++ truffle.js
@@ -1,12 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const cwd = path.resolve('.');
+
 module.exports = {
   networks: {
+
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4000000,
       gasPrice: 10000000000, // 10 gwei
-    }
+    },
+
+    volta: {
+      network_id: "73799",
+      gasPrice: "1",
+      confirmations: 1,
+      from: process.env.FROM,
+      provider: () => new HDWalletProvider(fs.readFileSync(path.resolve(cwd, process.env.SECRET_PATH), 'utf-8').trim(), 'https://volta-rpc.energyweb.org'),
+      production: true
+    },
+
+    ewc: {
+      network_id: "246",
+      gasPrice: "1",
+      confirmations: 1,
+      from: process.env.FROM,
+      provider: () => new HDWalletProvider(fs.readFileSync(path.resolve(cwd, process.env.SECRET_PATH), 'utf-8').trim(), 'https://rpc.energyweb.org'),
+      production: true
+    },
   },
   compilers: {
      solc: {
